e77d3a63097873ecd7d82f7ec1c55bf5ebff6054
malicesand <malicesand@gmail.com>
2024-09-11 21:56:11 -0500

    day-3 > type.js and first-class-functions.js

--- day-3/first-class-functions.js
+++ day-3/first-class-functions.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+const { stubTrue } = require("lodash");
+
 /**
  * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
  */
@@ -11,10 +13,26 @@
  * return a Function that tests whether a given value is greater than the 
  * base.
  */
+
+/*
+* I: base to test against (String or Number)
+* O: return function
+* C: function tests whether a given value is greater than the base
+* E: n/a
+ */
 function createGreaterThanFilter(base) {
     // YOUR CODE BELOW HERE //
-    
-   
+    // create callback function greaterThanFilter that takes in a single parameter
+    return function greaterThanFilter(value) {
+        //create conditional chain to compare base and paramater
+        if (value > base) {
+            // return true if parameter is greater than base
+            return true
+        } else {
+            // else returns false
+            return false
+        }
+   }
     
     // YOUR CODE ABOVE HERE //
 }
@@ -25,11 +43,26 @@ function createGreaterThanFilter(base) {
  * base. (test means return true or false)
  * 
  */
+
+/*
+* I: base to test against (String or Number)
+* O: Function that tests whether a given value is less than the base
+* C: Function returns true or false
+* E: conditional chain
+ */
 function createLessThanFilter(base) {
     // YOUR CODE BELOW HERE //
-    
-  
-    
+    // create callback function lessThanFilter that takes in a single parameter
+    return function lessThanFilter(value) {
+        //create conditional chain to compare base and paramater
+        if (value < base) {
+            // return true if parameter is less than base
+            return true
+        } else {
+            // else returns false
+            return false
+        }
+   }   
     
     // YOUR CODE ABOVE HERE //
 }
@@ -41,12 +74,34 @@ function createLessThanFilter(base) {
  * 
  * This function needs to be case insensitive.
  */
+
+/*
+* I: single character
+* O: Function that tests Input character against first character of a given String
+* C: Function returns true or false?
+* E: Case insensitive
+ */
 function createStartsWithFilter(startsWith) {
     // YOUR CODE BELOW HERE //
-    
-    
-    
-    
+    // create new variable to contain uppercase startsWith
+        // assign value of startsWith.toUpperCase() 
+    var startsUp = startsWith.toUpperCase();
+    // create new variable to contain lowercase startsWith
+        // assign value of startsWith.toLowerCase()
+    var startsLow = startsWith.toLowerCase();
+    // create callback function startsWithFilter that takes in a String as the parameter
+    return function startsWithFilter(string) {
+        // create new array and assign it the value of string.split('')
+        var array = string.split('');
+        // create conditional statement to compare the character at first index to startsUp and startsLow
+        if (array[0] === startsUp || array[0] === startsLow) {
+            //return true if array[0] === startsUp or startsLow
+            return true
+        } else {
+            // else return false
+            return false
+        }
+    }
     // YOUR CODE ABOVE HERE //
 }
 
@@ -57,12 +112,35 @@ function createStartsWithFilter(startsWith) {
  * 
  * This function needs to be case insensitive.
  */
+/*
+* I: single character
+* O: Function that tests Input character against final character of a given String
+* C: Function returns true or false?
+* E: Case insensitive
+ */
 function createEndsWithFilter(endsWith) {
     // YOUR CODE BELOW HERE //
-    
-    
-    
-    
+    // create new variable to contain uppercase startsWith
+        // assign value of startsWith.toUpperCase() 
+    var endsUp = endsWith.toUpperCase();
+    // create new variable to contain lowercase startsWith
+        // assign value of startsWith.toLowerCase()
+    var endsLow = endsWith.toLowerCase();
+    // create callback function startsWithFilter that takes in a String as the parameter
+    return function endsWithFilter(string) {
+        // create new array and assign it the value of string.split('')
+        var array = string.split('');
+        // use revese() method to reverse elements in array 
+        array.reverse();
+        // create conditional statement to compare the final character at first index to endsUp and endsLow
+        if (array[0] === endsUp || array[0] === endsLow) {
+            //return true if array[0] === endsUp or endsLow
+            return true
+        } else {
+            // else return false
+            return false
+        }
+    }
     // YOUR CODE ABOVE HERE //
 }
 
@@ -73,12 +151,24 @@ function createEndsWithFilter(endsWith) {
  * TIP: You need to loop over the Strings, right? We need to pass each String to 
  * the modify Function, but we need to collect the results into some collection.
  */
+
+/*
+* I: Array of Strings, Function designed to modify a String
+* O: return the modified Array of Strings
+* C: loop over strings
+* E: 
+ */
 function modifyStrings(strings, modify) {
     // YOUR CODE BELOW HERE //
-    
-    
-    
-    
+    // create variable to contain output and assign in the value of an empty array
+    var output = []
+    // create a for loop to iterate through the input array
+    for (var i = 0; i < strings.length; i++) {
+        // push the modification of each iteratated string into the output array
+        output.push(modify(strings[i]));
+    }
+    // return output
+    return output
     // YOUR CODE ABOVE HERE //
 }
 
@@ -94,9 +184,33 @@ function modifyStrings(strings, modify) {
  * 
  * TIP: You need to loop over the Strings, right? And pass them to the test?
  */
+
+/**
+ * I: Array of Strings and Function designed to test the String 
+ * O: Returns a Boolean 
+ * C: True if all Strings pass the test
+ * E: Loop each string and pass each to test
+ */
 function allStringsPass(strings, test) {
     // YOUR CODE BELOW HERE //
-    
+    // create array to contain all strings that pass the test
+    var testArr = [];
+    // create for loop to iterate through the array
+    for (var i = 0; i < strings.length; i++) {
+        // create conditional statement which passes each array index through the test 
+        if (test(strings[i]) === true) {
+            // if condition is true for a given index, string is passed to the testArr
+            testArr.push(strings[i]);
+        }
+    }
+    // create conditional statement comparing length of testArr to input Array
+    if (testArr.length === strings.length) {
+        // return true if testArr is strictly equal to the input Array
+        return true;
+    } else {
+        // if different lengths, return false
+        return false;
+    }
     
     
     
--- day-3/type.js
+++ day-3/type.js
@@ -14,10 +14,19 @@
  * 
  * HINT: There is a method that can help with this.
  */
+
+/*
+I: single parameter
+O: boolean
+C: use array.isArray() method
+E: n/a
+ */
+
 function isArray(value) {
     // YOUR CODE BELOW HERE //
-    
-    
+    // invoke array.isArray() method passing value as parameter
+    return Array.isArray(value)
+    // return method
     
     
     // YOUR CODE ABOVE HERE //
@@ -36,12 +45,38 @@ function isArray(value) {
  * HINT: look up how to figure out if something is an instance of the Date object.
  * 
  */
+/*
+*I: single parameter
+*O: boolean
+*C: determine if condition typeof === 'object' is true for the input data type 
+*E: use conditional chain to eliminate arrays, null, dates 
+ */
+
+// create function isObject that takes in one parameter value and returns true or false dependent on data type of value
 function isObject(value) {
     // YOUR CODE BELOW HERE //
-    
+    // if condition Array.isArray(value) is met     
+    if (Array.isArray(value)) {
+        // return false
+        return false
+    // if condition value instanceof Date is met     
+    } if (value instanceof Date) {
+        // return false
+        return false
+    // if condition null is met for value    
+    } if (value === null) {
+        // return false
+        return false
+    // if condition typeof === 'object' is met     
+    } if (typeof value === 'object') {
+        // return true
+        return true
+    // else
+    } else {
+        // return false
+        return false
+    }
 
-    
-    
     
     // YOUR CODE ABOVE HERE //
 }
@@ -54,12 +89,34 @@ function isObject(value) {
  * 
  * TIP: Similar to isObject, but we must return true if the value is an Array.
  */
+/*
+*I: single parameter
+*O: boolean
+*C: if parameter data type is array or object intended as collection, return true 
+*E: use conditional chain to eliminate null values and dates
+ */
+
+// create function isObject passes one parameter against condition, returning boolean dependent upon data type
 function isCollection(value) {
     // YOUR CODE BELOW HERE //
-    
-    
-    
-    
+    // if condition value instanceof Date is met     
+    if (value instanceof Date) {
+        // return false
+        return false
+    // if condition null is met for value    
+    } if (value === null) {
+        // return false
+        return false
+    // if condition typeof === 'object' is met     
+    } if (typeof value === 'object') {
+        // return true
+        return true
+    // else
+    } else {
+        // return false
+        return false
+    }
+ 
     // YOUR CODE ABOVE HERE //
 }
 
@@ -82,10 +139,55 @@ function isCollection(value) {
  *    typeOf("javascript") -> "string"
  *    typeOf([1,2,3]) -> "array"
  */ 
+
+/*
+* I: single parameter called value
+* O: String 
+* C: invoke the methods typeof, instanceof, and isArray
+* E: arrays, null values, and dates require conditional chain and strict comparisons 
+**/
+
+// create function that takes in one value and returns its data type as a string value
 function typeOf(value) {
     // YOUR CODE BELOW HERE //
-    
-    
+    // if typeof strictly equals string
+    if (typeof value === 'string'){
+        // return string
+        return 'string';   
+    // if isArray condition is true
+    } if (Array.isArray(value)) {
+        // return array
+        return 'array';
+    // if typeof strictly equals number
+    } if (typeof value === 'number') {
+        // return 'number'
+        return 'number'
+    // if typeof strictly equals boolean
+    } if (typeof value === 'boolean') {
+        // return 'boolean'
+        return 'boolean'
+    // if value strictly equals null
+    } if (value === null) {
+        // return 'null'
+        return 'null'
+    // if typeof strictly equals function
+    } if (typeof value === 'function') {
+        // return 'function'
+        return 'function'
+    // if value is instanceof Date
+    } if (value instanceof Date) {
+        //return 'date'
+        return 'date'
+    // else if value strictly equals object
+    } else if (typeof value === 'object') {
+        // return object
+        return 'object'
+    // else
+    } else {
+        // return 'undefined'
+        return 'undefined'
+  }
+
     
     
     // YOUR CODE ABOVE HERE //
